5b8be53aa65c59c8e7db5752b440d4ae6d4f21d3
zaidkhan1123 <zk117854@gmail.com>
2022-10-27 01:24:19 +0500

    comit

---
+++ src/Todo.jsx
@@ -0,0 +1,87 @@
+import React,{useState} from 'react'
+import image from "./img.png"
+function Todo() {
+
+    const [input, setInput] = useState('');
+    const [item, setItem] = useState([]);
+    const [buttonDisabled, setButtondisabled] = useState(false);
+
+
+    
+    //   if(input.length > 1) {
+    //     setButtondisabled[false]
+    //   } else {
+    //     setButtondisabled[true]
+    //   }
+    // }
+
+
+
+function AddItems(){
+    setItem((item)=>{
+      const newitem = [...item,input]
+      setInput("");
+      return  newitem;
+    })
+     
+}
+   
+function itemDelet (i){
+  const updatedlist = item.filter((elem,id)=>{
+    return i!=id
+    
+  })
+  setItem(updatedlist)
+}
+
+function REmoveAll(){
+  setItem([])
+}
+   
+  return (
+    <div className="container-fluid">
+        <div className="row ">
+            <div className="col-md-12 text-center mt-5">
+                <h1 className='text fw-bold text-center mt-5'>Todo list</h1>
+                <input  type="text" placeholder='Add items'value={input} minLength={1} required onChange={(e)=>{setInput(e.target.value)}} /> 
+                <button className='btn' onClick={AddItems} >Add</button>
+                </div>
+        </div>
+         {item !=[] && item.map((data,i)=>{
+          return (
+            <div className="row" >
+            <div className="col-md-12 text-center">
+            <h3 className='span text-white' key={i}>{data}
+             <i class="fa-sharp fa-solid fa-trash " onClick={()=>itemDelet(i)} ></i> 
+              </h3>
+            
+            </div>
+        </div>
+        
+          );
+         })}
+         {item.length >= 1 ?   <div className="row">
+          <div className="col-md-12 text-center mt-5 me-3"><button onClick={REmoveAll} className='new-btn'>Remove All</button></div>
+         
+         </div> : ""
+           }
+       
+        
+        
+      
+        <div className="row">
+            <div className="col text-center mt-5">
+            <img alt="" src={image}/>
+            </div>
+        </div>
+    </div>
+  )
+  }
+
+export default Todo
+
+
+
+
+
+
