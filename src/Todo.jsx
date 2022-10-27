9a95ebf857a83a820a71920865feeb85b724c94f
zaidkhan1123 <zk117854@gmail.com>
2022-10-28 01:50:49 +0500

    vv

--- src/Todo.jsx
+++ src/Todo.jsx
@@ -1,14 +1,38 @@
 import React,{useState} from 'react'
 import image from "./img.png"
+import dark from "./dark-mode.png"
 function Todo() {
 
     const [input, setInput] = useState('');
     const [item, setItem] = useState([]);
-    const [buttonDisabled, setButtondisabled] = useState(false);
+    const [darkmood, setDarkmood] = useState({
+      color : "white",
+      backgroundColor : "black"
+     });
+     const [btntext, setbtntext] = useState('Enable Light Mode');
+
+
+function DarkmoodG(){
+if(darkmood.color == "white"){
+  setDarkmood({
+      color : "black",
+      backgroundColor : "white"})
+      setbtntext('Enable Dark Mode')
+}
+  
+
+else{
+  setDarkmood({
+    color : "white",
+    backgroundColor : "black"
+  })
+  setbtntext("Enable Light Mode")
+}
 
 
 
-
+}
+  
 
 function AddItems(){
     setItem((item)=>{
@@ -31,12 +55,27 @@ function itemDelet (i){
 function REmoveAll(){
   setItem([])
 }
-   
+
 
   return (
 
-    <div className="container">
-      
+    <div className="container-fluid" style={darkmood}>
+      <div className="row">
+        <div className="col mt-4">
+      <div className="form-check form-switch ">
+  <input 
+  onClick={DarkmoodG}
+    className="form-check-input"
+    type="checkbox"
+    role="switch"
+    id="flexSwitchCheckDefault"
+  />
+  <label className="form-check-label" htmlFor="flexSwitchCheckDefault">
+    <img className='darkimage' src={dark} alt="" /> {btntext}
+  </label>
+</div>
+</div>
+</div>
         <div className="row ">
             <div className="col-md-12  text-center mt-5">
                 <h1 className='text fw-bold text-center mt-5'>Todo list</h1>
@@ -49,7 +88,7 @@ function REmoveAll(){
             <div className="row" >
             <div className="col-md-12  text-center">
             <h3 className='span text-white' key={i}>{data}
-             <i class="fa-sharp fa-solid fa-trash " onClick={()=>itemDelet(i)} ></i> 
+             <i class="fa-sharp fa-solid fa-trash "  onClick={()=>itemDelet(i)} ></i> 
               </h3>
             
             </div>
@@ -65,7 +104,7 @@ function REmoveAll(){
 
           {item.length >=1 ? "" : <div className="row">
             <div className="col-md-12 text-center mt-5">
-            <img  alt="" src={image}/>
+            <img className='image' alt="" src={image}/>
             </div>   
 
         </div>}    
